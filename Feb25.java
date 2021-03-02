83594abe9d02eb78b948b317287b9bef96341cbb
kumarpiyush1749 <77970919+kumarpiyush1749@users.noreply.github.com>
2021-03-02 10:55:42 +0530

    Add files via upload

---
+++ Feb25.java
@@ -0,0 +1,24 @@
+package feb;
+
+public class Feb25 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		float d=45;
+		int hey;
+		char f;
+		System.out.println("pity");
+
+		System.out.println("humdo humare do");
+		System.out.println(" piyush kumar is abad boy ");
+		System.out.println(" hello boy ");
+		System.out.println(" there are five boys");
+		System.out.println(" houmweceo");
+		System.out.println("howreh are you are");
+		System.out.println("a+b");
+		System.out.println("hello boy");
+		System.out.println("piyush klomv");
+		System.out.println("worn clothed");
+	}
+}
